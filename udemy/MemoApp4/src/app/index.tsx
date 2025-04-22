26ab79c9b00132afa3f68c478a42f972c3f5d599
shuri <okamoto.shuri.childrensday.555@gmail.com>
2025-04-22 10:59:32 +0900

    Install expo-router

---
+++ udemy/MemoApp4/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Index = (): JSX.Element => {
+    return(
+        <View>
+            <Text>Hello Worrd</Text>
+        </View>
+    )
+}
+
+export default Index

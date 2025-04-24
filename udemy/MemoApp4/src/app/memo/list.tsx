ffe8edd3e77a2aaf330c42d8007dd101374156e2
shuri <okamoto.shuri.childrensday.555@gmail.com>
2025-04-24 14:12:06 +0900

    create red icon

--- udemy/MemoApp4/src/app/memo/list.tsx
+++ udemy/MemoApp4/src/app/memo/list.tsx
@@ -1,9 +1,9 @@
 import { View, StyleSheet } from 'react-native'
-import { Feather } from '@expo/vector-icons'
 
 import Header from '../../components/Header'
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
+import Icon from '../../components/Icon'
 
 const List = (): JSX.Element => {
     return(
@@ -18,7 +18,7 @@ const List = (): JSX.Element => {
             </View>
 
             <CircleButton>
-                <Feather name='plus' size={40} />
+                <Icon />
             </CircleButton>
 
         </View>

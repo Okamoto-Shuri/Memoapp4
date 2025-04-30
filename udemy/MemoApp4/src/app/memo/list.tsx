3cb27cbc75aa015a2372bc845acf3ac26dae305f
shuri <okamoto.shuri.childrensday.555@gmail.com>
2025-04-30 14:35:04 +0900

    create back end a little

--- udemy/MemoApp4/src/app/memo/list.tsx
+++ udemy/MemoApp4/src/app/memo/list.tsx
@@ -1,11 +1,13 @@
 import { View, StyleSheet } from 'react-native'
 import { router, useNavigation } from 'expo-router'
 import { useEffect } from 'react'
+import { collection, query, onSnapshot, orderBy } from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import LogOutButton from '../../components/LogOutButton'
+import { db, auth } from '../../config'
 
 const handlePress = () => {
     router.push('/memo/create')
@@ -19,6 +21,17 @@ const List = (): JSX.Element => {
             return <LogOutButton />
         }
     })
+    }, [])
+    useEffect(() => {
+        if( auth.currentUser === null) { return }
+        const ref = collection(db, `user/${auth.currentUser.uid}/memos`)
+        const q = query(ref, orderBy('updatedAt', 'desc'))
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            snapshot.forEach((doc) => {
+                console.log('memo', doc.data())
+            })
+        })
+        return unsubscribe
     }, [])
     return(
         <View style={styles.container}>
